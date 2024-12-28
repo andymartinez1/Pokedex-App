b3e577df798d24fa02bce37e05449250c1f9e417
Andrew Martinez <andy72391@gmail.com>
2024-12-27 19:28:29 -0500

    Created the skeleton of the project. It uses Axios to fetch 20 pokemon per page and uses pagination to go to the next or previous page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,56 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./App.css";
+import Pagination from "./components/Pagination";
+import PokemonList from "./components/PokemonList";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [pokemon, setPokemon] = useState();
+  const [currentPageUrl, setCurrentPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [prevPageUrl, setPrevPageUrl] = useState();
+  const [loading, setLoading] = useState(true);
 
-  return null;
+  useEffect(() => {
+    setLoading(true);
+    let cancel;
+    axios
+      .get(currentPageUrl, {
+        cancelToken: new axios.CancelToken((c) => (cancel = c)),
+      })
+      .then((res) => {
+        setLoading(false);
+        setNextPageUrl(res.data.next);
+        setPrevPageUrl(res.data.previous);
+        setPokemon(res.data.results.map((p) => p.name));
+      });
+
+    return () => {
+      cancel();
+    };
+  }, [currentPageUrl]);
+
+  function goToNextPage() {
+    setCurrentPageUrl(nextPageUrl);
+  }
+
+  function goToPrevPage() {
+    setCurrentPageUrl(prevPageUrl);
+  }
+
+  if (loading) return "Loading...";
+
+  return (
+    <div>
+      <PokemonList pokemon={pokemon} />
+      <Pagination
+        goToNextPage={nextPageUrl ? goToNextPage : null}
+        goToPrevPage={prevPageUrl ? goToPrevPage : null}
+      />
+    </div>
+  );
 }
 
 export default App;
